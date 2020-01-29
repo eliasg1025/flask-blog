77eede122d7ba1047bab867465b0f4679777a847
Elias Guere <eliasguere1025@gmail.com>
2020-01-29 14:22:23 -0500

    Creando proyecto y generando las vista para registrar usuario

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '401ae2acb0b11be0dcbdae906846b31f'
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.jinja', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.jinja', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.jinja', title="Register", form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.jinja', title="login", form=form)
+
+
+# Init app
+if __name__ == '__main__':
+    app.run(debug=True)
